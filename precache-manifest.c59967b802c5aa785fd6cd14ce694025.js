263458720d675fb8b4b88ccb52b5f908d3fd1580
avalos010 <luizavalos40@gmail.com>
2019-01-11 18:22:11 -0600

    Updates

---
+++ precache-manifest.c59967b802c5aa785fd6cd14ce694025.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "43e1105bb713e1c5ab53",
+    "url": "/github-recipe/static/css/main.ec97e4af.chunk.css"
+  },
+  {
+    "revision": "43e1105bb713e1c5ab53",
+    "url": "/github-recipe/static/js/main.43e1105b.chunk.js"
+  },
+  {
+    "revision": "1d612dd27b42840fc66f",
+    "url": "/github-recipe/static/js/1.1d612dd2.chunk.js"
+  },
+  {
+    "revision": "974fc2c07a6321a77106",
+    "url": "/github-recipe/static/js/runtime~main.974fc2c0.js"
+  },
+  {
+    "revision": "b15a75e67ecb23a23f355ce4bfe02ec0",
+    "url": "/github-recipe/static/media/background-search.b15a75e6.jpg"
+  },
+  {
+    "revision": "ce357317815133655295f657bea9c497",
+    "url": "/github-recipe/index.html"
+  }
+];
