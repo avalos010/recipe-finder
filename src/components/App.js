b35d3d9604f8c6047b912bcda64932b6d403dd84
avalos010 <luizavalos40@gmail.com>
2019-01-13 12:04:32 -0600

    did some clean u work made a sepetate component for the recipes

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
-import React,{useContext,useState,useEffect} from 'react';
+import React,{createContext,useState,useEffect} from 'react';
 import secrets from '../secret'
+import Recipe from './Recipe';
 import AOS from 'aos'
 
 import 'aos/dist/aos.css'
 import './app.css'
+
+export const DataCtx = createContext(null);
  
 const App = () => {
     const [data, setData] = useState(null);
@@ -31,21 +34,9 @@ const App = () => {
             <input className="term_input" value={term} onChange={handleInputChange}/>
             <a className="btn_search" href={term ? '#recipe_container' : null} onClick={fetchData}>Search</a>
             </div>
-
-            <div className="recipe_container" id="recipe_container">
-                {data ? data.hits.map((hit, index) => {
-                    return( 
-                    <div key={index} className="recipe" data-aos-delay="50" data-aos-duration="5000" data-aos="fade-down-left">
-                    <h4 className="recipe_name"> {hit.recipe.label}</h4>
-                    <img className="recipe_image" src={hit.recipe.image} alt={hit.recipe.label}/>
-                    <p className="recipe_diet">Diet: {hit.recipe.dietLabels[0]}</p>
-                    <p className="recipe_calories">Calories {Math.round(hit.recipe.calories)} </p>
-                    <a className="recipe_link" target="_blank" href={hit.recipe.url}>Visit</a>
-                    </div>
-                    )
-                }) : <p className="recipes_none">Recipes Will Appear here!</p>}
-            </div>
-
+            <DataCtx.Provider value={[data,setData]}>
+            {data ? <Recipe /> : <p className="recipes_none">Recipes Will Appear here!</p>}
+            </DataCtx.Provider>
         </div>
     )
 };
--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import { DataCtx } from './App';
 
 const Recipe = () => {
-    return {
-        
-    }
-}
+    let [data] = useContext(DataCtx);
+    return (
+        <div className="recipe_container" id="recipe_container">
+                {data.hits.map((hit, index) => {
+                    return( 
+                    <div key={index} className="recipe" data-aos-delay="50" data-aos-duration="5000" data-aos="fade-in">
+                    <h4 className="recipe_name"> {hit.recipe.label}</h4>
+                    <img className="recipe_image" src={hit.recipe.image} alt={hit.recipe.label}/>
+                    <p className="recipe_diet">Diet: {hit.recipe.dietLabels[0]}</p>
+                    <p className="recipe_calories">Calories {Math.round(hit.recipe.calories)} </p>
+                    <a className="recipe_link" target="_blank" href={hit.recipe.url}>Visit</a>
+                    </div>
+                    )
+                })}
+            </div> 
+    )
+}        
+
+export default Recipe;
