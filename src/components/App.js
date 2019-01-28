7c517805ca6a8acf717524293b6f1af4cdc5bf59
avalos010 <luizavalos40@gmail.com>
2019-01-27 18:28:26 -0600

    added prettier and eslint to make the code look better

--- src/components/App.js
+++ src/components/App.js
@@ -1,44 +1,63 @@
-import React,{createContext,useState,useEffect} from 'react';
-import secrets from '../secret'
-import Recipe from './Recipe';
-import AOS from 'aos'
+import React, { createContext, useState, useEffect } from "react";
+import secrets from "../secret";
+import Recipe from "./Recipe";
+import AOS from "aos";
 
-import 'aos/dist/aos.css'
-import './app.css'
+import "aos/dist/aos.css";
+import "./app.css";
 
 export const DataCtx = createContext(null);
- 
+
 const App = () => {
-    const [data, setData] = useState(null);
-    const [term, setTerm] = useState('');
+  const [data, setData] = useState(null);
+  const [term, setTerm] = useState("");
 
-    useEffect(() => {
-        document.title = 'Recipe-Finder'
-        AOS.init()
-    });
+  useEffect(() => {
+    document.title = "Recipe-Finder";
+    AOS.init();
+  });
 
-    const fetchData = async() => {
-        const result = await fetch(`https://api.edamam.com/search?q=${term}&app_id=${secrets.appId}&app_key=${secrets.appKey}&to=25`);
-        const json = await result.json();
-        setData(json)
-    }
+  const fetchData = async () => {
+    const result = await fetch(
+      `https://api.edamam.com/search?q=${term}&app_id=${
+        secrets.appId
+      }&app_key=${secrets.appKey}&to=25`
+    );
 
-    const handleInputChange = (e) => {
-        setTerm(e.target.value)
-    }
+    const json = await result.json();
+    setData(json);
+  };
 
-    return (
-        <div className="container">
-            <div className="input_container">
-            <h1 className="title">Recipe Finder</h1>
-            <input className="term_input" value={term} onChange={handleInputChange}/>
-            <a className="btn_search" href={term ? '#recipe_container' : null} onClick={fetchData}>Search</a>
-            </div>
-            <DataCtx.Provider value={[data,setData]}>
-            {data ? <Recipe /> : <p className="recipes_none">Recipes Will Appear here!</p>}
-            </DataCtx.Provider>
-        </div>
-    )
+  const handleInputChange = e => {
+    setTerm(e.target.value);
+  };
+
+  return (
+    <div className="container">
+      <div className="input_container">
+        <h1 className="title">Recipe Finder</h1>
+        <input
+          className="term_input"
+          value={term}
+          onChange={handleInputChange}
+        />
+        <a
+          className="btn_search"
+          href={term ? "#recipe_container" : null}
+          onClick={fetchData}
+        >
+          Search
+        </a>
+      </div>
+      <DataCtx.Provider value={[data, setData]}>
+        {data ? (
+          <Recipe />
+        ) : (
+          <p className="recipes_none">Recipes Will Appear here!</p>
+        )}
+      </DataCtx.Provider>
+    </div>
+  );
 };
 
 export default App;
--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,23 +1,37 @@
-import React, {useContext} from 'react';
-import { DataCtx } from './App';
+import React, { useContext } from "react";
+import { DataCtx } from "./App";
 
 const Recipe = () => {
-    let [data] = useContext(DataCtx);
-    return (
-        <div className="recipe_container" id="recipe_container">
-                {data.hits.map((hit, index) => {
-                    return( 
-                    <div key={index} className="recipe" data-aos-delay="50" data-aos-duration="5000" data-aos="fade-in">
-                    <h4 className="recipe_name"> {hit.recipe.label}</h4>
-                    <img className="recipe_image" src={hit.recipe.image} alt={hit.recipe.label}/>
-                    <p className="recipe_diet">Diet: {hit.recipe.dietLabels[0]}</p>
-                    <p className="recipe_calories">Calories {Math.round(hit.recipe.calories)} </p>
-                    <a className="recipe_link" target="_blank" href={hit.recipe.url}>Visit</a>
-                    </div>
-                    )
-                })}
-            </div> 
-    )
-}        
+  let [data] = useContext(DataCtx);
+  return (
+    <div className="recipe_container" id="recipe_container">
+      {data.hits.map((hit, index) => {
+        return (
+          <div
+            key={index}
+            className="recipe"
+            data-aos-delay="50"
+            data-aos-duration="5000"
+            data-aos="fade-in"
+          >
+            <h4 className="recipe_name"> {hit.recipe.label}</h4>
+            <img
+              className="recipe_image"
+              src={hit.recipe.image}
+              alt={hit.recipe.label}
+            />
+            <p className="recipe_diet">Diet: {hit.recipe.dietLabels[0]}</p>
+            <p className="recipe_calories">
+              Calories {Math.round(hit.recipe.calories)}{" "}
+            </p>
+            <a className="recipe_link" target="_blank" href={hit.recipe.url}>
+              Visit
+            </a>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
 export default Recipe;
